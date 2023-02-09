692aa6930c5f7cc0d3c31b8f7a7d0cfd1c3c510a
Hector Arguello <arguellohector1@gmail.com>
2023-02-08 21:48:03 -0300

    first commit

---
+++ javascript/tipo-de-variable.js
@@ -0,0 +1,3 @@
+let x;       // Now x is undefined
+x = 5;       // Now x is a Number
+x = "John";  // Now x is a String
---
+++ javascript/variables.js
@@ -0,0 +1,5 @@
+let length=16;                                                      //Numero
+let lastnames="Johnson"                                             //String:Cadena de texto
+//let x={firstname:"John", lastname:"Doe"}                            //Objeto
+
+let x= 16 + "volvo";
