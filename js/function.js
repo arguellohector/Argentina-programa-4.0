12db8f3cff05dbe3419aa793584e0ac08dc05eb3
Hector Arguello <arguellohector1@gmail.com>
2023-02-08 21:54:42 -0300

    first commit

--- js/function.js
+++ js/function.js
@@ -1,8 +1,24 @@
+/**
+document.getElementById('boton').onclick=function(){
+    console.log("Capturamos el evento click");
+    document.getElementById('demo').innerHTML="Estamos probando nuestro primer evento en JS";
+}
+*/
+document.getElementById('boton').addEventListener('click', function(){
+    console.log("Hola mundo desde EventListener");
+    document.getElementById('demo').innerHTML="Estamos probando nuestro primer evento en JS";
+});
 
-console.log("Hola Mundo");
+document.getElementById('boton_color').addEventListener('click', function(){
+    document.body.style.backgroundColor ='#150EA9';
+  
+});
 
-let numero1=3;
-let numero2=4;
+document.getElementById('boton_default').addEventListener('click', function(){
+    document.body.style.backgroundColor ='yellowgreen';
+  
+});
+document.getElementById('boton_hora').addEventListener('click', function(){
+innerHTML=Date();
 
-let resultado= numero1+numero2;
-console.log("La suma de numero1 y numero2 es" + resultado);
+});
